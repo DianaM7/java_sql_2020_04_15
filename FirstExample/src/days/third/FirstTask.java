5dbc20eecadeb533978213134341897eb1f8aad7
DianaM7 <dianamarapolskiene@gmail.com>
2020-04-17 21:43:59 +0300

    third day third task

--- FirstExample/src/days/third/FirstTask.java
+++ FirstExample/src/days/third/FirstTask.java
@@ -1,24 +1,22 @@
 package days.third;
 
-import days.second.FirstTask;
 import days.second.NumberUtils;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class firstTask {
+public class FirstTask {
     private double height;
     private double weight;
     public static void main(String[] args) {
 
-        firstTask firstTask = new firstTask();
+        FirstTask firstTask = new FirstTask();
         firstTask.readRequiredUserData();
         DecimalFormat decimalFormat = new DecimalFormat("00.00");
 
         System.out.println("Jusu KMI: " + decimalFormat.format(firstTask.countKmi()));
 
     }
-
 
     private void readRequiredUserData(){
         Scanner scanner = new Scanner(System.in);
@@ -31,8 +29,5 @@ public class firstTask {
     private double countKmi(){
         return weight / Math.pow(height, 2);
     }
-
-
-
 
 }
